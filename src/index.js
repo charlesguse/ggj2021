e8f70eca0704a58c68594f98b381c6debcb44cdc
michaeljoy255 <kodiakchaos@gmail.com>
2021-01-30 18:55:19 -0500

    removed unneeded files and PWA stuff

--- src/index.js
+++ src/index.js
@@ -5,31 +5,6 @@ import MainMenu from './MainMenu'
 import Story from './Story'
 import Game from './Game'
 
-var enablePWA = false;
-if(enablePWA) {
-	// SERVICE WORKER
-	if('serviceWorker' in navigator) {
-		navigator.serviceWorker.register('./js/sw.js');
-	};
-	// NOTIFICATIONS TEMPLATE
-	Notification.requestPermission().then(function(result) {
-		if(result === 'granted') {
-			exampleNotification();
-		}
-	});
-	function exampleNotification() {
-		var notifTitle = 'GGJ 2021 Find Me!';
-		var notifBody = 'Created by the Enclave Games team.';
-		var notifImg = 'img/icons/icon-512.png';
-		var options = {
-			body: notifBody,
-			icon: notifImg
-		}
-		var notif = new Notification(notifTitle, options);
-		setTimeout(exampleNotification, 30000);
-	}
-}
-
 var gameConfig = {
 	scale: {
 		parent: 'ggj2021-app',
@@ -42,9 +17,3 @@ var gameConfig = {
 }
 const game = new Phaser.Game(gameConfig);
 window.focus();
-
-// Usage tracking
-// window.dataLayer = window.dataLayer || [];
-// function gtag(){dataLayer.push(arguments);}
-// gtag('js', new Date());
-// gtag('config', 'UA-30485283-26');
